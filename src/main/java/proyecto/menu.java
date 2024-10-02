ca706b2417effbccd6a6cb5e4bd6d8399badfaeb
DarkDoctor96 <camilohi.1196@gmail.com>
2024-10-01 21:53:30 -0500

    Menus
    Corrección de Mayúsculas y interacción del menú

---
+++ src/main/java/proyecto/menu.java
@@ -0,0 +1,36 @@
+package proyecto;
+
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("Bienvenido a Menu del Proyecto");
+            System.out.println("Menú Principal");
+            System.out.println("1. Iniciar Sesión");
+            System.out.println("2. Registro");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingresando a Iniciar Sesión ...");
+                    break;
+                case 2:
+                    System.out.println("Ingresando a Registro ...");
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        } while (opcion != 3);
+
+        scanner.close();
+    }
+}
